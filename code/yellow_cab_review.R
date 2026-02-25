33544b12636484cc4ec5972a327b53c4cf20d143
Guilherme Machado <machado.guilherme@live.com>
2026-02-25 19:25:50 -0300

    Formata dados

--- code/yellow_cab_review.R
+++ code/yellow_cab_review.R
@@ -10,8 +10,18 @@ library(scales)
 yellow <- open_dataset("C:/Users/macha/OneDrive/Desktop/projetos/taxi/data") %>%
   collect()
 
+#########
+yellow <- yellow %>%
+  separate(tpep_pickup_datetime,
+           into = c("pickup_date", "pickup_time"),
+           sep = " ") %>%
+  separate(tpep_dropoff_datetime,
+           into = c("dropoff_date", "dropoff_time"),
+           sep = " ")
 
-
+# Set a coluna de data como data format
+yellow$pickup_date <- as.Date(yellow$pickup_date)
+yellow$dropoff_date <- as.Date(yellow$dropoff_date)
 
 
 
