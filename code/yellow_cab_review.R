04a33492b43233f4af07ccef29f04b218e2d26e9
Guilherme Machado <machado.guilherme@live.com>
2026-02-25 19:02:45 -0300

    Pega libs necessarias

--- code/yellow_cab_review.R
+++ code/yellow_cab_review.R
@@ -1,7 +1,18 @@
 # Taxi grupo
 
 library(arrow)
+library(ggcorrplot)
 library(dplyr)
+library(tidyr)
+library(hms)
+library(scales)
 
 yellow <- open_dataset("C:/Users/macha/OneDrive/Desktop/projetos/taxi/data") %>%
   collect()
+
+
+
+
+
+
+
